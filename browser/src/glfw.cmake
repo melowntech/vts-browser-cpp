2de35f2c4f2ce51eadbf7c639311834ee596c8f3
Tomáš Malý <tomas.maly@melown.com>
2017-04-07 17:46:05 +0200

    fixed install

--- browser/src/glfw.cmake
+++ browser/src/glfw.cmake
@@ -2,6 +2,7 @@
 option(GLFW_BUILD_EXAMPLES "Build the GLFW example programs" OFF)
 option(GLFW_BUILD_TESTS "Build the GLFW test programs" OFF)
 option(GLFW_BUILD_DOCS "Build the GLFW documentation" OFF)
+option(GLFW_INSTALL "Generate installation target" OFF)
 
 add_subdirectory(src/glfw)
 include_directories(src/glfw/include)

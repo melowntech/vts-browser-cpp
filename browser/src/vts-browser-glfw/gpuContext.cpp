4871b90e1e977d765d83732d8613a2ea0b022c82
Tomáš Malý <tomas.maly@melown.com>
2017-08-17 18:32:02 +0200

    disable mipmap generation

--- browser/src/vts-browser-glfw/gpuContext.cpp
+++ browser/src/vts-browser-glfw/gpuContext.cpp
@@ -363,7 +363,6 @@ void GpuTextureImpl::loadTexture(vts::ResourceInfo &info,
     glTexImage2D(GL_TEXTURE_2D, 0, findInternalFormat(spec),
                  spec.width, spec.height, 0,
                  findFormat(spec), GL_UNSIGNED_BYTE, spec.buffer.data());
-    glGenerateMipmap(GL_TEXTURE_2D);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);

abfee2a10174a021734be4ae976ce19cd7b21792
Tomáš Malý <tomas.maly@melown.com>
2019-05-07 16:24:12 +0200

    fix geodata glsl nan handling for devices that have non-conformant float implementations

--- browser/src/vts-librenderer/data/shaders/geodata.inc.glsl
+++ browser/src/vts-librenderer/data/shaders/geodata.inc.glsl
@@ -35,16 +35,16 @@ float testVisibility(vec4 visibilities, vec3 modelPos, vec3 modelUp)
 {
     vec3 pos = vec3(uniMv * vec4(modelPos, 1.0));
     float distance = length(pos);
-    if (visibilities[0] == visibilities[0] && distance > visibilities[0])
+    if (!isnan(visibilities[0]) && distance > visibilities[0])
         return 0.0;
     distance *= 2.0 / uniProj[1][1];
-    if (visibilities[1] == visibilities[1] && distance < visibilities[1])
+    if (!isnan(visibilities[1]) && distance < visibilities[1])
         return 0.0;
-    if (visibilities[2] == visibilities[2] && distance > visibilities[2])
+    if (!isnan(visibilities[2]) && distance > visibilities[2])
         return 0.0;
     vec3 up = vec3(uniMv * vec4(modelUp, 0.0));
     vec3 f = -pos / distance;
-    if (visibilities[3] == visibilities[3]
+    if (!isnan(visibilities[3])
         && dot(f, up) < visibilities[3])
         return 0.0;
     return 1.0;

a613b8adf89b6eedb34320fa3b6320605e9bc59e
Tomáš Malý <tomas.maly@melown.com>
2019-06-28 09:58:30 +0200

    prefer to zoom out to speed up translation [wip] - oscilates and generally may misbehave

--- browser/src/vts-libbrowser/navigation/solver.cpp
+++ browser/src/vts-libbrowser/navigation/solver.cpp
@@ -71,6 +71,11 @@ double clamp2(double v, double a, double b)
     return clamp(v, std::min(a, b), std::max(a, b));
 }
 
+double vectorAbsSum(const vec3 &v)
+{
+    return std::abs(v[0]) + std::abs(v[1]) + std::abs(v[2]);
+}
+
 // perceptively invariant horizontal autopilot
 // piha = freckle in czech
 void piha(
@@ -87,6 +92,25 @@ void piha(
     double &outVerticalMove,
     vec3 &outRotation)
 {
+    assert(timeDiff >= 0);
+    assert(inHorizontalDistance >= 0);
+    assert(inStartViewExtent > 0);
+    assert(inStartViewExtent + inViewExtentChange > 0);
+
+    // no operation needed
+    if (inHorizontalDistance < 1e-7
+        && std::abs(inVerticalChange) < 1e-7
+        && std::abs(inViewExtentChange) < 1e-7
+        && vectorAbsSum(inRotationChange) < 1e-7)
+    {
+        outViewExtent = inStartViewExtent;
+        outHorizontalMove = 0;
+        outVerticalMove = 0;
+        outRotation = inStartRotation;
+        return;
+    }
+
+    // fps compensation
     double extentChangeFactor = multFactor(navOpts, timeDiff,
         navOpts.navigationPihaViewExtentChange + 1);
     double positionChangeFactor = addFactor(navOpts, timeDiff,
@@ -97,6 +121,7 @@ void piha(
     assert(positionChangeFactor > 0);
     assert(rotationChangeFactor > 0);
 
+    // translations (as a combination of horizontal and vertical movement)
     double translationSum = inHorizontalDistance
         + std::abs(inVerticalChange);
     double translationDist = length(vec2(inHorizontalDistance,
@@ -108,8 +133,10 @@ void piha(
     double verticalChange = inVerticalChange * translationFactor;
     translationDist = length(vec2(horizontalChange, verticalChange));
 
+    // rotation
     double rotationDist = length(inRotationChange);
 
+    // steps (frames) to complete individual operations
     double extentSteps = std::abs(std::log((inStartViewExtent
         + inViewExtentChange) / inStartViewExtent))
         / std::log(extentChangeFactor);
@@ -121,6 +148,40 @@ void piha(
     assert(translationSteps >= 0);
     assert(rotationSteps >= 0);
 
+    // zoom-out to speed up translation
+    if (translationSteps > 1e-7)
+    {
+        static const double piHalf = 3.14159265358979323846264338327 * 0.5;
+        static const double piQuarter = 3.14159265358979323846264338327 * 0.25;
+        double a = std::atan2(translationSteps, extentSteps);
+        assert(a >= 0 && a <= piHalf);
+        double b = piQuarter - std::abs(a - piQuarter);
+        assert(b >= 0 && b <= piQuarter);
+        b = std::pow(b / piQuarter, 0.5) * piQuarter;
+        assert(b >= 0 && b <= piQuarter);
+        double d = length(vec2(translationSteps, extentSteps));
+        double t = std::sin(b) * d;
+        double z = std::cos(b) * d;
+        assert(t >= 0);
+        assert(z >= 0);
+        //assert(std::abs(t + z - translationSteps - extentSteps) < 1e-5);
+
+        // update translation
+        double tf = t / translationSteps;
+        horizontalChange *= tf;
+        verticalChange *= tf;
+        translationSteps = t;
+
+        // update view extent
+        double e = std::exp(z * std::log(extentChangeFactor))
+            * inStartViewExtent - inStartViewExtent;
+        if (a < piQuarter && inViewExtentChange < 0)
+            e *= -1;
+        inViewExtentChange = e;
+        extentSteps = z;
+    }
+
+    // inter-operation normalization
     double normalizationSum = extentSteps + translationSteps + rotationSteps;
     double normalizationDist = length(vec3(extentSteps,
         translationSteps, rotationSteps));
@@ -133,12 +194,12 @@ void piha(
     double totalStepsInv = 1.0 / totalSteps;
     assert(totalStepsInv >= 0 && totalStepsInv <= 1);
 
-    double extentFactor = std::pow(extentChangeFactor, extentSteps * totalStepsInv);
+    // find the final outputs
+    double extentFactor = std::pow(extentChangeFactor,
+        extentSteps * totalStepsInv);
     if (inViewExtentChange < 0)
         extentFactor = 1.0 / extentFactor;
     outViewExtent = inStartViewExtent * extentFactor;
-    //outViewExtent = clamp2(outViewExtent, inStartViewExtent,
-    //    inStartViewExtent + inViewExtentChange);
     outHorizontalMove = horizontalChange * totalStepsInv;
     outVerticalMove = verticalChange * totalStepsInv;
     outRotation = inStartRotation + inRotationChange * totalStepsInv;

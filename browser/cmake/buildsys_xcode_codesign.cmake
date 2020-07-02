29de53e543c7f1f6845d05b642f0765388a8a5bc
Tomáš Malý <tomas.maly@melown.com>
2020-07-02 10:53:48 +0200

    ios: use development team id from environment; bump up minimum required cmake version to 3.10

--- browser/cmake/buildsys_xcode_codesign.cmake
+++ browser/cmake/buildsys_xcode_codesign.cmake
@@ -1,7 +1,7 @@
 
 function(buildsys_xcode_codesign target)
   if (BUILDSYS_IOS)
-    set(DEVELOPMENT_TEAM_ID "" CACHE STRING "Your TeamID Eg. 2347GVV3KC")
+    set(DEVELOPMENT_TEAM_ID "$ENV{DEVELOPMENT_TEAM_ID}" CACHE STRING "Your TeamID Eg. 2347GVV3KC")
     if (DEVELOPMENT_TEAM_ID)
       set_target_properties(${target} PROPERTIES
         XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY "iPhone Developer"

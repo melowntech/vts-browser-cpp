61e941818a5bc911f784d449cad2cb0472f10d50
Tomáš Malý <tomas.maly@melown.com>
2020-08-05 10:51:05 +0200

    option to explicitly disable implicit codesign for ios

--- browser/cmake/buildsys_xcode_codesign.cmake
+++ browser/cmake/buildsys_xcode_codesign.cmake
@@ -1,12 +1,14 @@
 
 function(buildsys_xcode_codesign target)
-  if (BUILDSYS_IOS)
+  message(STATUS "attempting to codesign ${target}")
+  if (BUILDSYS_IOS AND NOT BUILDSYS_IOS_NO_CODESIGN_ID)
     set(DEVELOPMENT_TEAM_ID "$ENV{DEVELOPMENT_TEAM_ID}" CACHE STRING "Your TeamID Eg. 2347GVV3KC")
     if (DEVELOPMENT_TEAM_ID)
       set_target_properties(${target} PROPERTIES
         XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY "iPhone Developer"
         XCODE_ATTRIBUTE_DEVELOPMENT_TEAM ${DEVELOPMENT_TEAM_ID}
       )
+      message(STATUS "codesign enabled for ${target}")
     endif()
   endif()
 endfunction(buildsys_xcode_codesign)
